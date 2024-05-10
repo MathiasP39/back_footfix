82e99a6091021249daad69b169bafff12336d7e3
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-05-10 10:18:52 +0200

    Ajout d'un Dockerfile fontionnel

--- app/controllers/articles_controller.ts
+++ app/controllers/articles_controller.ts
@@ -11,13 +11,13 @@ export default class ArticlesController {
     const query = await Article.query().orderBy('created_at', 'asc')
     return response.status(200).json(query)
   }
-//Function that handle the creation of an article
+  //Function that handle the creation of an article
   async publishArticles({ request, response }: HttpContext) {
     console.log(request)
     //validator
     return response.status(201).json({ message: 'Article created' })
   }
-//Function that handle the delete of an article
+  //Function that handle the delete of an article
   async deleteArticle({ request, response }: HttpContext) {
     console.log(request)
     return response.status(200).json({ message: 'article successfully deleted' })
