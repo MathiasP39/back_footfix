739c9c8b28b08beef5c2341822f6dbfb52ff3392
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-05-25 14:56:51 +0200

    Fix, Ajout du fichier de predeploy dans le docker

--- app/script/predeploy.sh
+++ app/script/predeploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo 'Starting predeploy script...'
 node ace db:wipe
 node ace migration:run --force
-echo 'Migrations complete.'
 node ace db:seed 
-echo 'Seeding complete.'
-echo 'predeploy Complete'
